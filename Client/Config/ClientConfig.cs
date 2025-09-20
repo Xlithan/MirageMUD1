ea85551335887d472aeeceaef0d35d7c8b0b2fa0
DraigNET Studios <steve.jones487@gmail.com>
2025-09-20 05:19:46 +0100

    Added encryption class and project config classes

--- Client/Config/ClientConfig.cs
+++ Client/Config/ClientConfig.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using MirageMUD.Shared.Utilities;
 
-namespace Client.Config
+namespace MirageMUD.Client.Config
 {
-    internal class ClientConfig
+    public sealed class ClientConfig
     {
+        public string ServerHost { get; set; } = "127.0.0.1";
+        public int ServerPort { get; set; } = 7777;
+
+        public bool Fullscreen { get; set; } = false;
+        public int MusicVolume { get; set; } = 70; // 0..100
+        public int SfxVolume { get; set; } = 70;   // 0..100
+
+        // Optional: simple keybind map (string names to keys)
+        public Dictionary<string, string> Keybinds { get; set; } = new()
+        {
+            ["MoveUp"] = "W",
+            ["MoveDown"] = "S",
+            ["MoveLeft"] = "A",
+            ["MoveRight"] = "D",
+            ["Attack"] = "Space"
+        };
+
+        public static string BaseDir =>
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MirageMUD");
+
+        public static string PathFile => System.IO.Path.Combine(BaseDir, "client.config.json");
+
+        public static ClientConfig Load() =>
+            JsonConfig.LoadOrCreate(PathFile, () => new ClientConfig());
+
+        public void Save() => JsonConfig.Save(PathFile, this);
     }
 }
--- Shared/Utilities/JsonConfig.cs
+++ Shared/Utilities/JsonConfig.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-namespace Shared.Utilities
+namespace MirageMUD.Shared.Utilities
 {
-    internal class JsonConfig
+    public static class JsonConfig
     {
+        private static readonly JsonSerializerOptions Options = new()
+        {
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        public static T LoadOrCreate<T>(string path, Func<T> createDefault)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+
+            if (!File.Exists(path))
+            {
+                var fresh = createDefault();
+                Save(path, fresh);
+                return fresh;
+            }
+
+            var json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<T>(json, Options)
+                   ?? throw new InvalidOperationException($"Failed to parse config {path}");
+        }
+
+        public static void Save<T>(string path, T data)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+            var json = JsonSerializer.Serialize(data, Options);
+            File.WriteAllText(path, json);
+        }
     }
 }
